c3ef373dc2b4688610073a76899f3867c06ae085
Mario Esteban Uclés <mario.esteban@posteo.es>
2020-03-08 12:01:56 +0100

    refactor: fix warnings on app file and disable console.log eslint restriction

--- src/client/src/App.vue
+++ src/client/src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
-    <Header title="Dashboard" :logoSrc="logoSrc"/>
+    <Header title="Dashboard" :logoSrc="logoSrc" />
     <Dashboard :data="data" />
   </div>
 </template>
 
 <script>
-import Dashboard from './components/Dashboard.vue';
-import Header from './components/Header.vue';
+import Dashboard from "./components/Dashboard.vue";
+import Header from "./components/Header.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     Dashboard,
-    Header,
+    Header
   },
   data() {
     return {
@@ -26,35 +26,35 @@ export default {
         },
         odo: 0,
         speed: 0,
-        soc: 0,
+        soc: 0
       },
-      logoSrc: require('@/assets/logo.png'),
+      logoSrc: require("@/assets/logo.png")
     };
   },
   mounted() {
     // Perform connection to WebSocket
-    const url = 'ws://localhost:3000';
+    const url = "ws://localhost:3000";
     this.connectToWebsocket(url, this.onConnectToWebsocket);
   },
   methods: {
-    connectToWebsocket (url, onConnectToWebsocket) {
+    connectToWebsocket(url, onConnectToWebsocket) {
       const connection = new WebSocket(url);
 
       connection.onopen = () => {
-        console.log('Connection to WebSocket Succesful');
+        console.log("Connection to WebSocket Succesful");
       };
       connection.onmessage = ({ data }) => {
-        onConnectToWebsocket(data)
+        onConnectToWebsocket(data);
       };
       connection.onerror = error => {
         console.log(`Error on WebSocket connection: ${error}`);
       };
     },
-    onConnectToWebsocket (data) {
+    onConnectToWebsocket(data) {
       // Parse the data sent by the WebSocket
       const parsedData = JSON.parse(data);
       // Split the string of coordinates in an array of two elements
-      const parsedCoordinates = parsedData.gps.split('|');
+      const parsedCoordinates = parsedData.gps.split("|");
       // Save into variables all the data
       this.data.time = parseInt(parsedData.time);
       this.data.energy = parseFloat(parsedData.energy);
@@ -74,7 +74,11 @@ body,
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   background: #7ca363;
-  background: linear-gradient(0deg, rgba(124,163,99,1) 0%, rgba(160,228,116,1) 100%);
+  background: linear-gradient(
+    0deg,
+    rgba(124, 163, 99, 1) 0%,
+    rgba(160, 228, 116, 1) 100%
+  );
   background-attachment: fixed;
   height: 100vh;
   margin: 0;
