303848ed853abf4fa63285efb1332c80a0f52635
Mario Esteban Uclés <mario.esteban@posteo.es>
2020-03-08 21:31:08 +0100

    test: add unit test to verify that connect to socket on mount

--- src/client/tests/unit/App.spec.js
+++ src/client/tests/unit/App.spec.js
@@ -12,10 +12,28 @@ describe("App.vue", () => {
       // Stub LinearChart to fix failing tests because of canvas
       LinearChart: "<span></span>"
     };
+    const connectToWebsocket = jest.fn();
 
     const wrapper = shallowMount(App, { stubs });
     expect(wrapper.find(Header).exists()).toBeTruthy();
     expect(wrapper.find(Dashboard).exists()).toBeTruthy();
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it("connects to websocket on mounted", () => {
+    // Stub dynamically loaded components replacing them with their real components
+    const stubs = {
+      Header,
+      Dashboard,
+      // Stub LinearChart to fix failing tests because of canvas
+      LinearChart: "<span></span>"
+    };
+    const connectToWebsocket = jest.fn();
+    const methods = {
+      connectToWebsocket
+    };
+
+    const wrapper = shallowMount(App, { stubs, methods });
+    expect(connectToWebsocket).toHaveBeenCalledTimes(1);
   });
 });
